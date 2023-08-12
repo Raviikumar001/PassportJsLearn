0e0e3264a1fcb46589e463db90c81277132168bb
ravi <samcollins9899@gmail.com>
2023-08-12 22:55:00 +0530

    added view tempalte

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
-const express = require("express");
-const passport = require("passport");
-const session = require('express-session');
-require('dotenv').config();
-require("./auth");
+const express =require('express');
 
-console.log(process.env.GOOGLE_CLIENT_SECRET);
 const app = express();
-app.use(session({ secret: 'cats'}));
-app.use(passport.initialize())
-app.use(passport.session())
 
-function isLoggedIn(req,res, next)
-{
+app.set('view engine','ejs');
 
-    req.user ? next(): res.sendStatus(401);
-}
-
-app.get("/", (req, res) => {
-  res.send('<a href="/auth/google">Authenticate with google</a>');
-});
-
-
-app.get("/auth/google",
-
-passport.authenticate('google', {scope: ['email', 'profile']})
-);
-
-app.get('/google/callback',
- passport.authenticate('google',{
-    successRedirect: '/protected',
-    failureRedirect: './auth/failure',
- }))
- 
-
- app.get('/auth/failure', (req,res)=> {
-    res.send('something went wrong');
- })
-app.get("/protected", isLoggedIn,(req, res) => {
-  res.send(`Hello ${req.user.displayName}`);
-});
-
-app.get('/logout', (req,res)=>{
-
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        res.redirect('/');
-      });
-    req.session.destroy();
-    res.send('Goodbye!');
+app.get('/',(req,res)=>{
+  res.render("home")
 })
 
-app.listen(3000, () => console.log("listening on: 3000"));
+app.listen(3000,function(){
+  console.log('App is listening on port 3000');
+})
