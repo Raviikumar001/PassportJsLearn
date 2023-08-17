d61844839ea422f7b267e261688b0bb28d00db71
ravi <samcollins9899@gmail.com>
2023-08-17 22:47:25 +0530

    added profile page

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
-router.get('/', (req,res)=> {
-    res.send('you are looged in , this is your profile- ' +req.user.username)
+const authCheck = (req,res,next)=>{
+    if(!req.user)
+    { 
+        res.redirect('/auth/login')
+
+    }else {
+        // if logged in
+        next();
+    }
+}
+
+router.get('/',authCheck, (req,res)=> {
+    res.render('profile', {user:req.user})
 })
 
 
